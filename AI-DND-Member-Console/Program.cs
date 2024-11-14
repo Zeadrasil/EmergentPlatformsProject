7e96e62430802465ed9b78dc267cb7d4bdaf72e8
Curtis-Lane <129118489+Curtis-Lane@users.noreply.github.com>
2024-11-13 19:36:16 -0700

    Add project files.

---
+++ AI-DND-Member-Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AI_DND_Member_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
