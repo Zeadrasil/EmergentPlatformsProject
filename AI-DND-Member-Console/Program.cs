f8d5414b513a6659a243a0cebdf16b9ed18a9b2e
Curtis-Lane <129118489+Curtis-Lane@users.noreply.github.com>
2024-11-14 15:30:09 -0700

    Made some tweaks to formatting and wording

--- AI-DND-Member-Console/Program.cs
+++ AI-DND-Member-Console/Program.cs
@@ -5,6 +5,11 @@ using OllamaSharp;
 namespace AI_DND_Member_Console {
 	internal class Program {
 		static void Main(string[] args) {
+			Console.WriteLine("This is a chat space where you can interact with an AI and use it to play D&D,");
+			Console.WriteLine("with the AI serving either as a player or as a DM.");
+			Console.WriteLine("At any point, type \"/bye\" to end the chat.");
+			Console.Write('\n');
+
 			// Setup the client
 			Uri uri = new Uri("http://localhost:11434");
 			OllamaApiClient client = new OllamaApiClient(uri);
@@ -14,19 +19,18 @@ namespace AI_DND_Member_Console {
 			Chat chat = new Chat(client);
 
 			while(true) {
+				Console.Write(">> ");
 				string message = Console.ReadLine();
 
 				if(message == "/bye") {
 					break;
 				}
 
+				Console.Write('\n');
 				foreach(var answerToken in chat.SendAsync(message).ToBlockingEnumerable()) {
-					Console.Write('\n');
 					Console.Write(answerToken);
 				}
 				Console.Write("\n\n");
-
-				Console.WriteLine("Type \"/bye\" to end");
 			}
 		}
 	}
