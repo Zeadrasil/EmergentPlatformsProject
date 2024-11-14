ce76bae09e34365e7d22deb2dab06dfac73ee96a
Curtis-Lane <129118489+Curtis-Lane@users.noreply.github.com>
2024-11-14 15:13:33 -0700

    You can chat with the AI now

--- AI-DND-Member-Console/Program.cs
+++ AI-DND-Member-Console/Program.cs
@@ -1,18 +1,33 @@
 ﻿using System;
+using System.Threading.Tasks;
+using OllamaSharp;
 
-namespace AI_DND_Member_Console
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-            do
-            {
-                Console.WriteLine(Console.ReadLine());
-                Console.WriteLine("Press n to end");
-            }
-            while ((Console.ReadLine() != "n"));
-        }
-    }
+namespace AI_DND_Member_Console {
+	internal class Program {
+		static void Main(string[] args) {
+			// Setup the client
+			Uri uri = new Uri("http://localhost:11434");
+			OllamaApiClient client = new OllamaApiClient(uri);
+			client.SelectedModel = "qwen2.5:7b";
+
+			// Setup the chat
+			Chat chat = new Chat(client);
+
+			while(true) {
+				string message = Console.ReadLine();
+
+				if(message == "/bye") {
+					break;
+				}
+
+				foreach(var answerToken in chat.SendAsync(message).ToBlockingEnumerable()) {
+					Console.Write('\n');
+					Console.Write(answerToken);
+				}
+				Console.Write("\n\n");
+
+				Console.WriteLine("Type \"/bye\" to end");
+			}
+		}
+	}
 }
