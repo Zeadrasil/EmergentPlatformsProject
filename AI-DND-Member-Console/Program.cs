18339fb569d39aedf1d944a1c5e4b0a7c55b7519
Zeadrasil <thewitepheonix@gmail.com>
2024-11-20 16:18:59 -0700

    DM Selection
    Allows you to select whether you are playing as the DM or as a player, also built functionality so that we can use the AI to interpret answers to questions we ask the player

--- AI-DND-Member-Console/Program.cs
+++ AI-DND-Member-Console/Program.cs
@@ -6,13 +6,44 @@ namespace AI_DND_Member_Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Will you be operating as the DM for this session?");
+            //do
+            //{
+            //    Console.WriteLine(Console.ReadLine());
+            //    Console.WriteLine("Press n to end");
+            //}
+            //while ((Console.ReadLine() != "n"));
+            Testing.Run();
+            string answer = Console.ReadLine();
+            if (Testing.ProcessTrueFalseQuestion("Will you be acting as the DM?", answer) || Testing.ProcessTrueFalseQuestion("What role will you be playing?", "they are a Dungeon Master", answer))
+            {
+                RunAsDM();
+            }
+            else
+            {
+                RunAsPlayer();
+            }
+        }
+
+        static void RunAsDM()
+        {
+            string answer;
             do
             {
-                Console.WriteLine(Console.ReadLine());
-                Console.WriteLine("Press n to end");
+                Console.WriteLine("Would you like to exit the application (DM)?");
+                answer = Console.ReadLine();
             }
-            while ((Console.ReadLine() != "n"));
+            while (!Testing.ProcessTrueFalseQuestion("Would you like to exit the application?", answer) && !Testing.ProcessTrueFalseQuestion("What would you like to do next?", "they would like to exit the application", answer));
+        }
+        static void RunAsPlayer()
+        {
+            string answer;
+            do
+            {
+                Console.WriteLine("Would you like to exit the application (Player)?");
+                answer = Console.ReadLine();
+            }
+            while (!Testing.ProcessTrueFalseQuestion("Would you like to exit the application?", answer) && !Testing.ProcessTrueFalseQuestion("What would you like to do next?", "they would like to exit the application", answer));
         }
     }
 }
