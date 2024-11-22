fc004cf95ba8d407e7f169220550b686df9b2b0b
Zeadrasil <thewitepheonix@gmail.com>
2024-11-21 19:12:32 -0700

    Minor changes
    Minor changes to prompts to make it easier for the AI to recognize what abilities you are referring to

--- AI-DND-Member-Console/Program.cs
+++ AI-DND-Member-Console/Program.cs
@@ -9,7 +9,7 @@ namespace AI_DND_Member_Console
         static void Main(string[] args)
         {
             Console.WriteLine("Will you be operating as the DM for this session?");
-            //Testing.Run();
+            Testing.Run();
             string answer = Console.ReadLine();
             if (Testing.ProcessTrueFalseQuestion("Will you be acting as the DM?", answer) || Testing.ProcessTrueFalseQuestion("What role will you be playing?", "they are a Dungeon Master", answer))
             {
@@ -42,9 +42,9 @@ namespace AI_DND_Member_Console
                 sheet.abilities.Add(ability);
                 Console.WriteLine(Testing.GetResponse($"Please summarize the following DnD character sheet in a readable format: {sheet.ToString()}"));
                 Console.WriteLine("Use an ability:");
-                Console.WriteLine(Testing.GetResponse($"Please use the ability \"{Console.ReadLine()}\" on the following character sheet: {sheet.ToString()}. If the character has an ability with a similar name, please respond with only the ability name, and if they do not respond with \"No Matching Ability\"."));
+                Console.WriteLine(Testing.GetResponse($"Please use the ability \"{Console.ReadLine()}\" on the following character sheet: {Testing.GetResponse($"Please respond with the following character sheet except with the name of the \"Smash\" ability replaced with the name \"Hulk Smash\": {sheet.ToString()}")}. If the character has an ability with a similar name, please respond with only the ability name, and if they do not respond with \"No Matching Ability\"."));
                 Console.WriteLine("Use a weapon:");
-                Console.WriteLine(Testing.GetResponse($"Please use the weapon \"{Console.ReadLine()}\" on the following character sheet: {sheet.ToString()}. If the character has a weapon with a similar name, please respond with only the weapon name, and if they do not respond with \"No Matching Weapon\"."));
+                Console.WriteLine(Testing.GetResponse($"Please use the weapon \"{Console.ReadLine()}\" on the following character sheet: {Testing.GetResponse($"Please respond with the following character sheet except with the name of the \"Smash\" ability replaced with the name \"Hulk Smash\": {sheet.ToString()}")}. If the character has a weapon with a similar name, please respond with only the weapon name, and if they do not respond with \"No Matching Weapon\"."));
                 Console.WriteLine(Testing.GetResponse($"Please respond with the following character sheet except with the name of the \"Smash\" ability replaced with the name \"Hulk Smash\": {sheet.ToString()}"));
                 Console.WriteLine(Testing.GetResponse($"Please generate a character sheet similar but not the same as the following at level 1: {sheet.ToString()}"));
                 Console.WriteLine("Would you like to exit the application (DM)?");
