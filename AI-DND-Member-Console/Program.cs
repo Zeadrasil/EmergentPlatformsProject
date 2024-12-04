98f9cc5b83e09ca8e8c6772b154a0641167505fa
Curtis-Lane <129118489+Curtis-Lane@users.noreply.github.com>
2024-12-03 19:43:39 -0700

    Get basic saving and loading working

--- AI-DND-Member-Console/Program.cs
+++ AI-DND-Member-Console/Program.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using OllamaSharp;
 
@@ -8,44 +10,105 @@ namespace AI_DND_Member_Console
 {
 	internal class Program
 	{
-		internal static StringBuilder messagesBuilder = new StringBuilder();
+		private static StringBuilder messagesBuilder = new StringBuilder();
 		internal static LinkedList<string> messagesToSave = new LinkedList<string>();
 
-		internal static string SystemPromptAIDM = "You are the dungeon master for a game of D&D 5th edition. Act as the DM would, giving players the ability to act on their own while also enforcing the rules of the game. There is only one player in this session.";
-		internal static string SystemPromptAIPlayer = "You are a player in a game of D&D 5th edition. Act as a player would, rolling dice and roleplaying while trying to follow the rules to the best of your ability. You are the only player in this session.";
-		
+		private static StringBuilder memoryBuilder = new StringBuilder();
+
+		private static string SystemPromptAIDM = "You are the dungeon master for a game of D&D 5th edition. Act as the DM would, giving players the ability to act on their own while also enforcing the rules of the game. There is only one player in this session.";
+		private static string SystemPromptAIPlayer = "You are a player in a game of D&D 5th edition. Act as a player would, rolling dice and roleplaying while trying to follow the rules to the best of your ability. You are the only player in this session.";
+
+		private const string SAVE_FILE_LOCATION = "../../../saves";
+		private static SaveData? saveFile = null;
+		private static string saveFileName = string.Empty;
+
 		static void Main(string[] args)
 		{
 			Testing.Setup();
+
+			if(!Directory.Exists(SAVE_FILE_LOCATION))
+			{
+				Directory.CreateDirectory(SAVE_FILE_LOCATION);
+			}
+
 			Console.WriteLine("At any point, specify that you would like to exit the game to stop playing.");
 			Console.Write('\n');
-			Console.WriteLine("Will you be operating as the DM for this session?");
-			string answer = Console.ReadLine();
-			Console.Write('\n');
-			//if (Testing.ProcessTrueFalseQuestion("Will you be acting as the DM?", answer) || Testing.ProcessTrueFalseQuestion("What role will you be playing?", "they are a Dungeon Master", answer))
-			if(answer.ToLower() == "yes")
+			if(Directory.GetFiles(SAVE_FILE_LOCATION).Length > 0) {
+				Console.WriteLine("Is there a save file you would like to load?");
+				string loadAnswer = Console.ReadLine();
+				Console.Write('\n');
+
+				if(loadAnswer.ToLower() == "yes" || loadAnswer.ToLower() == "y") {
+					Console.WriteLine("What is the name of this save file?");
+					string? input = null;
+					while(input == null) {
+						input = Console.ReadLine();
+
+						// Ensure the file name is valid
+						if(input != null && input.IndexOfAny(Path.GetInvalidPathChars()) >= 0) {
+							input = null;
+						}
+					}
+
+					string json = File.ReadAllText($"{SAVE_FILE_LOCATION}/{input}.json");
+					saveFileName = input;
+
+					saveFile = JsonSerializer.Deserialize<SaveData>(json);
+				}
+			}
+
+			if(saveFile == null)
 			{
-				RunAsDM();
+				Console.WriteLine("Will you be operating as the DM for this session?");
+				string answer = Console.ReadLine();
+				Console.Write('\n');
+				if(answer.ToLower() == "yes" || answer.ToLower() == "y")
+				{
+					RunAsDM();
+				}
+				else
+				{
+					RunAsPlayer();
+				}
 			}
 			else
 			{
-				RunAsPlayer();
+				Console.Write('\n');
+				Console.WriteLine(saveFile.rawData);
+
+				if(saveFile.isDM)
+				{
+					memoryBuilder.AppendLine(SystemPromptAIPlayer);
+
+					foreach(string data in saveFile.summarizedData) {
+						memoryBuilder.AppendLine(data);
+					}
+
+					RunAsDM();
+				}
+				else
+				{
+					memoryBuilder.AppendLine(SystemPromptAIDM);
+
+					foreach(string data in saveFile.summarizedData) {
+						memoryBuilder.AppendLine(data);
+					}
+
+					RunAsPlayer();
+				}
 			}
 		}
 
 		static void RunAsDM()
 		{
-			messagesToSave.AddFirst(SystemPromptAIPlayer);
-
-			StringBuilder memoryBuilder = new StringBuilder();
-			memoryBuilder.AppendLine(SystemPromptAIPlayer);
+			//messagesToSave.AddFirst(SystemPromptAIPlayer);
 
 			string? answer = null;
 			do
 			{
 				if(answer != null)
 				{
-					messagesToSave.AddLast(answer);
+					//messagesToSave.AddLast(answer);
 					memoryBuilder.AppendLine(answer);
 				}
 
@@ -54,7 +117,7 @@ namespace AI_DND_Member_Console
 					messagesBuilder.Append(stream.Response);
 				}
 
-				messagesToSave.AddLast(messagesBuilder.ToString());
+				//messagesToSave.AddLast(messagesBuilder.ToString());
 				memoryBuilder.AppendLine(messagesBuilder.ToString());
 				messagesBuilder.Clear();
 
@@ -94,10 +157,9 @@ namespace AI_DND_Member_Console
 
 		static void RunAsPlayer()
 		{
-			messagesToSave.AddFirst(SystemPromptAIDM);
+			//messagesToSave.AddFirst(SystemPromptAIDM);
 
-			StringBuilder memoryBuilder = new StringBuilder();
-			memoryBuilder.AppendLine(SystemPromptAIDM);
+			bool skipFirstPrompt = saveFile != null;
 
 			string? answer = null;
 			do
@@ -108,14 +170,18 @@ namespace AI_DND_Member_Console
 					memoryBuilder.AppendLine(answer);
 				}
 
-				foreach(var stream in Testing.client.GenerateAsync((answer == null) ? SystemPromptAIDM : memoryBuilder.ToString()).ToBlockingEnumerable()) {
-					Console.Write(stream.Response);
-					messagesBuilder.Append(stream.Response);
-				}
+				if(!skipFirstPrompt)
+				{
+					foreach(var stream in Testing.client.GenerateAsync((answer == null) ? SystemPromptAIDM : memoryBuilder.ToString()).ToBlockingEnumerable()) {
+						Console.Write(stream.Response);
+						messagesBuilder.Append(stream.Response);
+					}
 
-				messagesToSave.AddLast(messagesBuilder.ToString());
-				memoryBuilder.AppendLine(messagesBuilder.ToString());
-				messagesBuilder.Clear();
+					messagesToSave.AddLast(messagesBuilder.ToString());
+					memoryBuilder.AppendLine(messagesBuilder.ToString());
+					messagesBuilder.Clear();
+				}
+				skipFirstPrompt = false;
 
 				Console.Write('\n');
 				Console.Write("\n>>> ");
@@ -123,6 +189,57 @@ namespace AI_DND_Member_Console
 				Console.Write('\n');
 			}
 			while (!Testing.ProcessTrueFalseQuestion("Would you like to exit the application?", answer) && !Testing.ProcessTrueFalseQuestion("What would you like to do next?", "they would like to exit the application", answer));
+
+			StringBuilder textToSummarize = new StringBuilder();
+			foreach(string message in messagesToSave)
+			{
+				textToSummarize.AppendLine(message);
+			}
+
+			StringBuilder summaryBuilder = new StringBuilder();
+			foreach(var stream in Testing.client.GenerateAsync($"Summarize the following string: {textToSummarize}").ToBlockingEnumerable())
+			{
+				summaryBuilder.Append(stream.Response);
+			}
+
+			if(saveFile == null)
+			{
+				saveFile = new SaveData();
+				saveFile.summarizedData.Add(summaryBuilder.ToString());
+				saveFile.rawData = memoryBuilder.ToString();
+
+				JsonSerializerOptions options = new JsonSerializerOptions();
+				options.WriteIndented = true;
+
+				string json = JsonSerializer.Serialize(saveFile, options);
+
+				Console.WriteLine("What would you like to name this save file?");
+				string? input = null;
+				while(input == null)
+				{
+					input = Console.ReadLine();
+
+					// Ensure the file name is valid
+					if(input != null && input.IndexOfAny(Path.GetInvalidPathChars()) >= 0)
+					{
+						input = null;
+					}
+				}
+
+				File.WriteAllText($"{SAVE_FILE_LOCATION}/{input}.json", json);
+			}
+			else
+			{
+				saveFile.summarizedData.Add(summaryBuilder.ToString());
+				saveFile.rawData = memoryBuilder.ToString();
+
+				JsonSerializerOptions options = new JsonSerializerOptions();
+				options.WriteIndented = true;
+
+				string json = JsonSerializer.Serialize(saveFile, options);
+
+				File.WriteAllText($"{SAVE_FILE_LOCATION}/{saveFileName}.json", json);
+			}
 		}
 	}
 }
